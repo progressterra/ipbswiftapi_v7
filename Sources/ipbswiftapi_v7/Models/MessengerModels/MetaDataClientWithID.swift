de1316c135eb5421b4eadfd33c33299894246222
Artemy Volkov <wasartemwolf@gmail.com>
2023-11-08 17:44:03 +0300

    MetaDataClientWithID listImages added

--- Sources/ipbswiftapi_v7/Models/MessengerModels/MetaDataClientWithID.swift
+++ Sources/ipbswiftapi_v7/Models/MessengerModels/MetaDataClientWithID.swift
@@ -11,12 +11,14 @@ public struct MetaDataClientWithID: Codable {
     public let dataSourceType: TypeDataSource
     public let dataSourceName: String?
     public let description: String?
+    public let listImages: [RGEntityMediaDataViewModel]?
     public let idClient: String
     
-    public init(dataSourceType: TypeDataSource, dataSourceName: String?, description: String?, idClient: String) {
+    public init(dataSourceType: TypeDataSource, dataSourceName: String?, description: String?, listImages: [RGEntityMediaDataViewModel]?, idClient: String) {
         self.dataSourceType = dataSourceType
         self.dataSourceName = dataSourceName
         self.description = description
+        self.listImages = listImages
         self.idClient = idClient
     }
 }
